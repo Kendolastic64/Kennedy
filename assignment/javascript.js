8470031e953af766ccb5fd224e2c14623654d292
Kendolastic64 <55988079+Kendolastic64@users.noreply.github.com>
2020-06-15 00:28:35 +0100

    Add files via upload

---
+++ assignment/javascript.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".hamburg");
+const navBar= document.querySelector(".header");
+
+
+menuIcon.addEventListener("click", () => {
+    navBar.classList.toggle('change')
+}) ;
